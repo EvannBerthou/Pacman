3864bb10a2e97e552af76c5661adc3288db245ca
Sutalite <nedje28@gmail.com>
2020-10-23 18:15:30 +0200

    Ajout de la structure entité

---
+++ src/entite.h
@@ -0,0 +1,34 @@
+#ifndef ENTITE_H
+#define ENTITE_H
+
+/* Structure Pos: permet de stocker un couple ligne/colonne                   */
+typedef struct pos {
+    int     l;
+    int     c;
+} Pos;
+
+
+typedef enum {
+    ENTITE_PACMAN = 0,
+    ENTITE_FANTOME
+} TypeEntite;
+
+/* Structure element permet de stocker le sens de deplacement le score de pacman et mode chase des fantome */
+typedef struct {
+    int sens;
+    int prochain_sens;
+    int score;
+    int fuite;
+    int nb_vie;
+} Etat;
+
+typedef struct {
+    Pos pos;
+    TypeEntite type;
+    Etat etat;
+    int delai_deplacement; // le temps restant avant que l'entité se déplace
+} Entite;
+
+Entite nouvelle_entite(Pos pos, TypeEntite type);
+
+#endif
---
+++ src/pacman.h
@@ -0,0 +1,9 @@
+#ifndef PACMAN_H
+#define PACMAN_H
+
+#include "partie.h"
+
+void nouveau_pacman();
+void bouger_pacman(Partie *p);
+
+#endif
