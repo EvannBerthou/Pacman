162f3c173c9c13f8a26ab6ad47ecd2c7f95a6a81
Sutalite <sutalite@DESKTOP-C0OBNN4.localdomain>
2020-11-06 17:04:49 +0100

    géré les erreurs dans les requetes

--- src/leaderboard.c
+++ src/leaderboard.c
@@ -8,33 +8,44 @@
 
 #include "leaderboard.h"
 
-// Fonctions d'aide pour gérer les erreurs
-static int err(int result, const char *msg) {
-    if (result < 0) {
-#ifdef DEBUG
-        fprintf(stderr, "%s\n", msg);
-#endif
-        exit(1);
-    }
-    return result;
-}
+/* 
+Ces macros servent a quitter la fonction envoyer_requete en cas d'erreur dans la requête
+elles permettent de direction renvoyer NULL en cas d'erreur et diminue la quantité de code répété
+pour la vérification de chaque erreur dans la fonction
+*/
 
-static void * errp(void *p, const char *msg) {
-    if (p == NULL) {
-#ifdef DEBUG
-        fprintf(stderr, "%s\n", msg);
-#endif
-        exit(1);
-    }
-    return p;
-}
+#define ERR(x, msg, r) do { \
+  int retval = (x); \
+  if (retval < 0) { \
+    fprintf(stderr, msg); \
+    close(sockfd); \
+    return NULL /* or throw or whatever */; \
+  } \
+  if (r != NULL) { \
+    (*r) = retval; \
+  } \
+} while (0)
+
+#define ERRP(x, msg, r) do { \
+  void *retval = (x); \
+  if (retval == NULL) { \
+    fprintf(stderr, msg); \
+    close(sockfd); \
+    return NULL /* or throw or whatever */; \
+  } \
+  (*r) = retval; \
+} while (0)
+
 
 // Envoie une requete à un serveur et renvoie la réponse du serveur
 char *envoyer_requete(const char *host, int port, const char *req) {
 
     // Création du socket
-    int sockfd = err(socket(AF_INET, SOCK_STREAM, 0), "Erreur lors de la création du socket");
-    struct hostent *server = errp(gethostbyname(host), "Serveur non trouvé");
+    int sockfd;
+    ERR(socket(AF_INET, SOCK_STREAM, 0), "Erreur lors de la création du socket", &sockfd);
+    // Trouver l'ip du serveur par rapport à son host
+    struct hostent *server;
+    ERRP(gethostbyname(host), "Serveur non trouvé", &server);
 
     // Initialisation du socket pour qu'il se connecte au serveur
     struct sockaddr_in addr;
@@ -44,14 +55,15 @@ char *envoyer_requete(const char *host, int port, const char *req) {
     memcpy(&addr.sin_addr.s_addr,server->h_addr,server->h_length);
 
     // Connection au serveur
-    err(connect(sockfd,(struct sockaddr *)&addr,sizeof(addr)), "Erreur lors de la connection");
-
+    int res;
+    ERR(connect(sockfd,(struct sockaddr *)&addr,sizeof(addr)), "Erreur lors de la connection", &res);
 
     // Envoie de la requete tout en gérant les éventuelles paquets perdus
     int total = strlen(req);
     int envoye = 0;
     do {
-        int bytes = err(send(sockfd, req + envoye, total - envoye, 0), "Erreur lors de l'envoie");
+        int bytes;
+        ERR(send(sockfd, req + envoye, total - envoye, 0), "Erreur lors de l'envoie", &bytes);
         if (bytes == 0) break; // Connection au serveur perdue
         envoye+=bytes;
     } while (envoye < total);
@@ -61,8 +73,9 @@ char *envoyer_requete(const char *host, int port, const char *req) {
     char *reponse = calloc(total, sizeof(char));
     int recu = 0;
     do {
-        int bytes = err(recv(sockfd, reponse + recu, total - recu, 0), "Erreur lors de la lecture");
-        if (bytes == 0) break; // Connection au serveurperdue
+        int bytes;
+        ERR(recv(sockfd, reponse + recu, total - recu, 0), "Erreur lors de la lecture", &bytes);
+        if (bytes == 0) break; // Connection au serveur perdue
         recu += bytes;
     } while (recu < total);
 
@@ -80,19 +93,21 @@ void afficher_leaderboard() {
     const char *get_req = 
             "GET / HTTP/1.0\r\n"
             "Host: pacman-leaderboard.herokuapp.com\r\n"
-            "Content-Type: application/x-www-form-urlencoded\r\n"
-            "Content-Length: %d\r\n\r\n"
-            "%s\r\n";
+            "Content-Type: application/x-www-form-urlencoded"
+            "\r\n\r\n";
 
-    char req[2048];
-    const char get_params[] = "joueur=c";
-    sprintf(req, get_req, strlen(get_params), get_params);
-
-    char *reponse = envoyer_requete("pacman-leaderboard.herokuapp.com", 80, req);
     //char *reponse = envoyer_requete("localhost", 3000, req);
+    char *reponse = envoyer_requete("pacman-leaderboard.herokuapp.com", 80, get_req);
+    // En cas d'erreur dans la requête
+    if (reponse == NULL) {
+        free(reponse);
+        return;
+    }
+
     char *body = strstr(reponse, "\r\n\r\n");
     char *state;
     char *line = strtok_r(body, "\r\n", &state);
+
     Point point_dessin = {0,0};
     while (line != NULL) {
         char *partie;
